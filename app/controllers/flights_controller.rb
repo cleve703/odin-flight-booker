b6abe58dc78ab26097c0c3bf5ad902f17d75651b
jay <cleve703@gmail.com>
2020-05-30 04:12:07 -0400

    fix search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,13 @@
 class FlightsController < ApplicationController
 
   def index
-    @flights = Flight.all
+    if params[:origin_airport].present?
+      @flights = Flight.where(origin: params[:origin_airport],
+                              destination: params[:destination_airport], 
+                              departure_time: Date.strptime(params[:departure_date], "%m/%d/%Y").all_day)
+    else
+      @flights = Flight.all
+    end
   end
 
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,8 +2,8 @@ class Flight < ApplicationRecord
   belongs_to :origin, :class_name => 'Airport', :foreign_key => 'origin_id'
   belongs_to :destination, :class_name => 'Airport', :foreign_key => 'destination_id'
 
-  def self.flight_dates
+  def self.departure_dates_formatted
     Flight.all.map { |f| f.departure_time.to_date.strftime("%m/%d/%Y") }.uniq.sort
-  end 
+  end
 
 end
