c47c48fb6452370489c2105087e74bf7ed8f4c49
jay <cleve703@gmail.com>
2020-06-01 18:08:16 -0400

    add radio button to flights and default values in search criteria

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,8 +5,16 @@ class FlightsController < ApplicationController
       @flights = Flight.where(origin: params[:origin_airport],
                               destination: params[:destination_airport], 
                               departure_time: Date.strptime(params[:departure_date], "%m/%d/%Y").all_day)
+      @search_criteria = { origin_airport: params[:origin_airport] || '1', 
+                           destination_airport: params[:destination_airport] || '1', 
+                           departure_date: params[:departure_date] || '06/01/2020', 
+                           num_passengers: params[:num_passengers] || '1' }
     else
       @flights = Flight.all
+      @search_criteria = { origin_airport: '1', 
+        destination_airport: '1', 
+        departure_date: '06/01/2020', 
+        num_passengers: '1' }
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 airports = Airport.create([{ airport_code: 'IAD' }, { airport_code: 'SEA' }, { airport_code: 'CHS' }, { airport_code: 'DCA' }, { airport_code: 'BWI' }, { airport_code: 'LAX' }])
-flights = Flight.create([{ departure_time: DateTime.civil(2020, 6, 1, 21, 30, 00), duration: 90, origin_id: 1, destination_id: 2},
+flights = Flight.create([{ departure_time: DateTime.civil(2020, 6, 1, 21, 30, 00), duration: 300, origin_id: 1, destination_id: 2},
                          { departure_time: DateTime.civil(2020, 6, 1, 07, 30, 00), duration: 30, origin_id: 1, destination_id: 3}, 
                          { departure_time: DateTime.civil(2020, 6, 1, 11, 00, 00), duration: 30, origin_id: 1, destination_id: 4},
                          { departure_time: DateTime.civil(2020, 6, 1, 21, 30, 00), duration: 30, origin_id: 4, destination_id: 5},
                          { departure_time: DateTime.civil(2020, 6, 1, 21, 30, 00), duration: 300, origin_id: 5, destination_id: 6},
                          { departure_time: DateTime.civil(2020, 6, 1, 21, 30, 00), duration: 300, origin_id: 6, destination_id: 2},
+                         { departure_time: DateTime.civil(2020, 6, 1, 21, 30, 00), duration: 300, origin_id: 3, destination_id: 2},
+                         { departure_time: DateTime.civil(2020, 6, 1, 21, 30, 00), duration: 300, origin_id: 2, destination_id: 4},
                          { departure_time: DateTime.civil(2020, 6, 2, 11, 00, 00), duration: 30, origin_id: 1, destination_id: 4},
                          { departure_time: DateTime.civil(2020, 6, 3, 21, 30, 00), duration: 30, origin_id: 4, destination_id: 5},
+                         { departure_time: DateTime.civil(2020, 6, 3, 21, 30, 00), duration: 30, origin_id: 2, destination_id: 5},
+                         { departure_time: DateTime.civil(2020, 6, 3, 21, 30, 00), duration: 300, origin_id: 3, destination_id: 6},
                          { departure_time: DateTime.civil(2020, 6, 3, 21, 30, 00), duration: 300, origin_id: 5, destination_id: 6},
-                         { departure_time: DateTime.civil(2020, 6, 4, 21, 30, 00), duration: 300, origin_id: 6, destination_id: 2},])
+                         { departure_time: DateTime.civil(2020, 6, 4, 21, 30, 00), duration: 300, origin_id: 6, destination_id: 2}])
