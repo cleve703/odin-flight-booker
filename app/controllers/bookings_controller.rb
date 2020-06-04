b128dfd33a760feab233faa0e82c65bd9ed42d65
jay <cleve703@gmail.com>
2020-06-04 12:43:52 -0400

    add strong params to booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,25 @@
 class BookingsController < ApplicationController
   def new
-    @flight = Flight.find_by(id: params[:booking][:flight_id].to_i)
-    @flight_id = @flight.id
+    @flight = Flight.find_by(id: booking_params[:flight_id].to_i)
+    @flight_id = booking_params[:flight_id]
     @booking = @flight.bookings.new(flight_id: @flight_id)
-    params[:booking][:num_passengers].to_i.times {@booking.passengers.build}
+    booking_params[:num_passengers].to_i.times {@booking.passengers.build}
   end
 
   def create
-    @flight = Flight.find_by(id: params[:booking][:flight_id].to_i)
+    @flight = Flight.find_by(id: booking_params[:flight_id].to_i)
     @booking = @flight.bookings.create!(flight_id: @flight.id)
-    @booking.create_associated_passengers(params[:booking][:passengers_attributes])
+    @booking.create_associated_passengers(booking_params[:passengers_attributes])
     render :show
   end
 
-  def show(booking)
-    @booking = booking
+  def show
   end
+
+  private
+
+    def booking_params
+      params.require(:booking).permit(:flight_id, :num_passengers, :passengers_attributes, passengers_attributes: [:name, :email])
+    end
 
 end
