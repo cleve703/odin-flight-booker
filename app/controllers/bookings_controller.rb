7f1ff81f2232b4f2c8004a7b42bedebc21a4e08b
jay <cleve703@gmail.com>
2020-06-04 12:12:38 -0400

    add strong params for flight search

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,13 @@
 class BookingsController < ApplicationController
   def new
-    @flight = Flight.find_by(id: params[:flight].to_i)
+    @flight = Flight.find_by(id: params[:booking][:flight_id].to_i)
     @flight_id = @flight.id
-    @booking = @flight.bookings.new(flight_id: @flight.id)
-    params[:num_passengers].to_i.times {@booking.passengers.build}
+    @booking = @flight.bookings.new(flight_id: @flight_id)
+    params[:booking][:num_passengers].to_i.times {@booking.passengers.build}
   end
 
   def create
-    @flight = Flight.find_by(id: params[:flight_id].to_i)
+    @flight = Flight.find_by(id: params[:booking][:flight_id].to_i)
     @booking = @flight.bookings.create!(flight_id: @flight.id)
     @booking.create_associated_passengers(params[:booking][:passengers_attributes])
     render :show
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,24 +1,30 @@
 class FlightsController < ApplicationController
 
   def index
-    if params[:origin_airport].present?
-      @flights = Flight.where(origin: params[:origin_airport],
-                              destination: params[:destination_airport], 
-                              departure_time: Date.strptime(params[:departure_date], "%m/%d/%Y").all_day).order(:departure_time).paginate(page: params[:page])
-      @search_criteria = { origin_airport: params[:origin_airport], 
-                           destination_airport: params[:destination_airport], 
-                           departure_date: params[:departure_date], 
-                           num_passengers: params[:num_passengers] }
-      @selected_num_passengers = params[:num_passengers].to_i
+    if params[:flight].present?
+      @flights = Flight.where(origin: search_params[:origin_airport],
+                              destination: search_params[:destination_airport], 
+                              departure_time: Date.strptime(search_params[:departure_date], "%m/%d/%Y").all_day).order(:departure_time).paginate(page: params[:page])
+      @search_criteria = { origin_airport: search_params[:origin_airport], 
+                           destination_airport: search_params[:destination_airport], 
+                           departure_date: search_params[:departure_date], 
+                           num_passengers: search_params[:num_passengers] }
+      @selected_num_passengers = search_params[:num_passengers].to_i
       @select = true
     else
       @flights = Flight.order(:departure_time).paginate(page: params[:page])
       @search_criteria = { origin_airport: '1', 
         destination_airport: '1', 
-        departure_date: '06/01/2020', 
+        departure_date: Date.today, 
         num_passengers: '1' }
       @select = false
     end
   end
+
+  private
+
+    def search_params
+      params.require(:flight).permit(:origin_airport, :destination_airport, :num_passengers, :departure_date)
+    end
 
 end
